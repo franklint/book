f81fad0e367115ccadc68bd957283c28ae242b1f
franklinT <franklintsung3@gmail.com>
2022-07-31 19:23:32 -0400

    third

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,49 +1,48 @@
-const { gql } = require("apollo-server-express"); 
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-
-type Query{ 
+  type Query {
     me: User
-}
+  }
 
-type user{ 
-    _id: ID! 
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID!
     username: String
     email: String
     bookCount: Int
     savedBooks: [Book]
-}
+  }
 
-type Book { 
+  type Book {
     bookId: String
     authors: [String]
     description: String
     title: String
     image: String
     link: String
-}
+  }
 
-type Mutation{ 
-    login(email: String!, password: String!): Auth 
-    addUser(username: String!, email: String!, password: String!): Auth 
-    savedBook(input: savedBook!): User 
-    removeBook(bookId: ID!): User
-}
-
-type Auth { 
-    token: ID!
-    user: User
-}
-
-input savedBook{ 
-    description: String 
+  input savedBook {
+    description: String
     title: String
     bookId: String
     image: String
     link: String
     authors: [String]
-}
+  }
+  
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
 
-`; 
-
-module.exports = typeDefs; 
+// export the typeDefs
+module.exports = typeDefs;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,27 +1,31 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({req}) {
-    // allows token to be sent via  req.query or headers
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
+  authMiddleware: function ({ req }) {
+    //allows token to be sent via req.body, req.query, or headers
+
     let token = req.body.token || req.query.token || req.headers.authorization;
-
-    console.log("TOKEN", token); 
-
-    // ["Bearer", "<tokenvalue>"]
+    // let token = req.query.token || req.headers.authorization;
+    console.log("TOKEN", token);
+    //separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
+    //if no token, return request object as is
     if (!token) {
-      return req; 
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       //decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
@@ -30,11 +34,8 @@ module.exports = {
     } catch {
       console.log("Invalid token");
     }
-    // send to next endpoint
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    //return updated request object
+    return req;
   },
 };
